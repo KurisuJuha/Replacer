3aca1c8c7a90d26ef94ef444ab00a414921c6e3b
KYapp <kukiyomenaisapp@gmail.com>
2023-01-31 22:36:05 +0900

    どれか一つを選ぶOneRuleを作った

---
+++ Assets/Replacer/Runtime/Rules/OneRule.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace JuhaKurisu.PopoTools.Replacer
+{
+    public class OneRule<T> : Rule<T>
+    {
+        readonly Rule<T>[] rules;
+
+        public OneRule(params Rule<T>[] rules)
+        {
+            this.rules = rules;
+        }
+
+        public override IEnumerator<bool> Step(T[,] values)
+        {
+            Rule<T> rule = rules[Random.Range(0, rules.Length)];
+
+            IEnumerator<bool> e = rule.Step(values);
+
+            while (e.MoveNext())
+            {
+                yield return true;
+            }
+        }
+    }
+}
