39652f37e01f69658a9e27d783c845d2010e8073
KYapp <kukiyomenaisapp@gmail.com>
2023-01-31 18:17:25 +0900

    Scan関数の追加

--- Assets/Replacer/Runtime/Rule.cs
+++ Assets/Replacer/Runtime/Rule.cs
@@ -1,9 +1,28 @@
 using System.Collections.Generic;
+using JuhaKurisu.PopoTools.Extentions;
 
 namespace JuhaKurisu.PopoTools.Replacer
 {
     public abstract class Rule<T>
     {
         public abstract IEnumerator<T[,]> Step(T[,] values);
+        public bool Scan(T[,] values, T[,] before, (int x, int y) position)
+        {
+            for (int x = 0; x < before.GetLength(0); x++)
+            {
+                for (int y = 0; y < before.GetLength(1); y++)
+                {
+                    if (!values.IsIndexWithInRange(position.x + x, position.y + y)) return false;
+                    if (!before.IsIndexWithInRange(x, y)) return false;
+
+                    if (!values[position.x + x, position.y + y].Equals(before[x, y]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
     }
 }
