6fb7d67d715aed09dd7f3acfaa5be06bea4168a7
KYapp <kukiyomenaisapp@gmail.com>
2023-01-31 18:17:34 +0900

    ScanAllの追加

--- Assets/Replacer/Runtime/Rule.cs
+++ Assets/Replacer/Runtime/Rule.cs
@@ -6,6 +6,23 @@ namespace JuhaKurisu.PopoTools.Replacer
     public abstract class Rule<T>
     {
         public abstract IEnumerator<T[,]> Step(T[,] values);
+
+        public (int x, int y)[] ScanAll(T[,] values, T[,] before)
+        {
+            List<(int x, int y)> ret = new List<(int x, int y)>();
+
+            for (int x = 0; x < values.GetLength(0); x++)
+            {
+                for (int y = 0; y < values.GetLength(1); y++)
+                {
+                    if (!Scan(values, before, (x, y))) continue;
+                    ret.Add((x, y));
+                }
+            }
+
+            return ret.ToArray();
+        }
+
         public bool Scan(T[,] values, T[,] before, (int x, int y) position)
         {
             for (int x = 0; x < before.GetLength(0); x++)
