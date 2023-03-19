e9ebf146c14c5d5231e804c2bc20882e6b441cf3
KYapp <kukiyomenaisapp@gmail.com>
2023-03-19 19:26:08 +0900

    test

--- Assets/Test/Generator.cs
+++ Assets/Test/Generator.cs
@@ -10,36 +10,12 @@ namespace JuhaKurisu
         public IEnumerator Generate(Color[,] colors)
         {
             colors.Fill(Color.black);
-            colors[colors.GetLength(0) / 2, colors.GetLength(1) / 2] = Color.red;
+            colors[colors.GetLength(0) / 2, colors.GetLength(1) / 2] = Color.white;
 
-            Rule<Color> rule = new SequenceRule<Color>(
-                new WhileRule<Color>(
-                    new RandomReplaceRule<Color>(
-                        new Color[] {
-                            Color.red,
-                            Color.black,
-                            Color.black
-                        },
-                        new Color[] {
-                            Color.white,
-                            Color.white,
-                            Color.red
-                        }
-                    )
-                ),
-                new WhileRule<Color>(
-                    new RandomReplaceRule<Color>(
-                        new Color[] {
-                            Color.white,
-                            Color.white,
-                            Color.red
-                        },
-                        new Color[] {
-                            Color.red,
-                            Color.black,
-                            Color.black
-                        }
-                    )
+            Rule<Color> rule = new WhileRule<Color>(
+                new RandomReplaceRule<Color>(
+                    new Color[] { Color.white, Color.black },
+                    new Color[] { Color.white, Color.white }
                 )
             );
 
